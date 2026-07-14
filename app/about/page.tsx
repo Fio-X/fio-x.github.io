8696fb8ed1f0995c9aaf7178c20ef98f9ee35559
Origin <fio@OMBP.local>
2026-07-14 16:39:29 +0800

    Add return link to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function AboutPage() {
-  return <h1>关于我</h1>
+  return (
+    <main>
+      <h1>关于我</h1>
+      <Link href="/">返回首页</Link>
+    </main>
+  );
 }
