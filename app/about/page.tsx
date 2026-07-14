4652f2a50339046be526540ffb14621a651dfe88
Origin <fio@OMBP.local>
2026-07-14 17:14:50 +0800

    Add spacing to about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function AboutPage() {
   return (
-    <main>
+    <main className="space-y-4 p-8">
       <h1>关于我</h1>
       <Link href="/">返回首页</Link>
     </main>
